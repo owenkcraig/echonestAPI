24340d1374c7ffd43c85b05c03a18ebdd4fdf655
Owen Craig <owenkcraig@gmail.com>
2016-02-25 18:25:39 -0500

    Clears display on form submit

--- javaScript/script.js
+++ javaScript/script.js
@@ -11,13 +11,6 @@ app.apiSimilarArtistsUrl = 'http://developer.echonest.com/api/v4/playlist/static
 app.apiHottestSongsUrl = 'http://developer.echonest.com/api/v4/song/search';
 app.apiSongSummaryUrl = 'http://developer.echonest.com/api/v4/song/profile';
 
-
-//Temporary variables; to delete later
-// var artist = 'radiohead';
-// var userWorkout = 120;
-
-//End of temporary variables
-
 // Return list of similar artists
 app.getArtists = function() {
 	$.ajax({
@@ -38,22 +31,15 @@ app.getArtists = function() {
 		}
 
 	}).then(function(artists){
-		// var artistIDs = [];
-		// for (i = 0; i < 8; i++) {
-		// 	artistIDs.push(artists.response.artists[i].id)
-		// }
 		var response = artists;
 		app.displayPlaylist(response);
 	});
 };
 // Grab artist ID for similar artists
 app.displayPlaylist = function(filteredSongDetails) {
-		// console.log(filteredSongDetails[0].artist_name);
-		// console.log(filteredSongDetails);
+	$('#results').html('');
 	var songs = filteredSongDetails.response.songs;
-	// console.log(songs)
-			var songTitle = '';
-			// var /songArtist = '';
+	var songTitle = '';
 		$.each(songs, function(i, songDetails){
 			var songTitle = $('<h3>').text(songDetails.title);
 			var songArtist = $('<h3>').text(songDetails.artist_name);
@@ -62,117 +48,14 @@ app.displayPlaylist = function(filteredSongDetails) {
 		});
 	};
 
-// Search through hottest songs by artist ID
-// app.getHotSongs = function(artistIDs) {
-// 	var songCalls = artistIDs.map(function(id){
-// 		return $.ajax({
-// 			url: app.apiHottestSongsUrl,
-// 			datatype: 'json',
-// 			method: 'GET',
-// 			data: {
-// 				api_key: app.apiKey,
-// 				artist_id: id,
-// 				sort: 'song_hotttnesss-desc',
-// 				results: '4'
-// 			}
-// 		});
-// 	});
-// 	// Grab song ID for each song ID
-// 	$.when.apply(null,songCalls)
-// 		.then(function() {
-// 			var songArray = Array.prototype.slice.call(arguments);
-// 			var songIDs = [];
-// 			songArray = songArray.map(function(song) {
-// 				return song[0].response.songs;
-// 			})
-// 			songArray = songArray.map(function(artistSongs) {
-// 				var count = 0;
-// 				return artistSongs.filter( function(song){
-// 					count += 1;
-// 					if(count <= 10 ) {
-// 						return true;
-// 					} else {
-// 						return false;
-// 					}
-// 				})
-// 			})
-// 			songArray = _.flatten(songArray);
-// 			$.each(songArray, function( i,songs) {
-// 				var hotttsong = songs.id;
-// 				songIDs.push(hotttsong);
-// 			})
-// 			app.songCheck(songIDs);
-// 		});
-// };
-
-
-//Search all songs by song ID
-// app.songCheck = function(songIDs) {
-// 	var songDetails = songIDs.map(function(id){
-// 		return $.ajax({
-// 			url: app.apiSongSummaryUrl,
-// 			datatype: 'json',
-// 			method: 'GET',
-// 			data: {
-// 				api_key: app.apiKey,
-// 				id: id,
-// 				bucket: 'audio_summary'
-// 			}
-// 		});
-// 	});
-// 	$.when.apply(null,songDetails)
-// 		.then(function() {
-// 			var songDetails = Array.prototype.slice.call(arguments);	
-// 			var filteredSongDetails = [];
-// 			$.each(songDetails, function( i,songBPMs) {
-// 				if(songDetails[i][0].response.songs[0].audio_summary.tempo >= app.userWorkout &&
-// 					songDetails[i][0].response.songs[0].audio_summary.tempo <= app.userWorkout + 20){
-// 					filteredSongDetails.push(songDetails[i][0].response.songs[0])
-// 				}
-// 			})
-// 			filteredSongDetails = songDetails.filter(function(song) {
-// 				// console.log(song)
-// 				return song[0].response.songs[0].audio_summary.tempo >= app.userWorkout;
-// 			});
-// 			filteredSongDetails = app.shuffle(filteredSongDetails);
-// 			filteredSongDetails = filteredSongDetails.map(function(song) {
-// 				return song[0].response.songs[0];
-// 			})
-// 			app.displayPlaylist(filteredSongDetails);
-// 		});
-
-	
-
-// Filter out songs that do not match BPM criteria
-
-// Build a sample playlist for user
-
-// Display playlist in working functionality
-
-// app.shuffle = function(songDetails) {
-// 	var m = songDetails.length, t, i;
-
-// 	// While there remain elements to shuffle…
-// 				  while (m) {
-
-// 				    // Pick a remaining element…
-// 				    i = Math.floor(Math.random() * m--);
-
-// 				    // And swap it with the current element.
-// 				    t = songDetails[m];
-// 				    songDetails[m] = songDetails[i];
-// 				    songDetails[i] = t;
-// 				  }
-
-// 				  return songDetails;
-// 				};
-
 app.init = function() {
 	$('form').on('submit', function(e) {
 		e.preventDefault();
 		app.userInput = $('input[type=text]').val();
 		app.userWorkoutMin = $('input[type=radio]:checked').val();
-		app.userWorkoutMax = 200;
+		app.userWorkoutMax = parseFloat(app.userWorkoutMin) + parseFloat(20);
+		console.log(app.userWorkoutMin);
+		console.log(app.userWorkoutMax);
 		app.getArtists();
 	});
 };
